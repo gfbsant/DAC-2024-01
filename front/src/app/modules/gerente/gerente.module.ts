109580bbeb16d92604e278a2b3a94832e8529e9e
Gabriel F. B. Santos <gfbsant@gmail.com>
2024-03-18 23:07:15 -0300

    elimina rejeitar cliente component da gerente.module

--- front/src/app/modules/gerente/gerente.module.ts
+++ front/src/app/modules/gerente/gerente.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeGerenteComponent } from './components/home-gerente/home-gerente.component';
-import { RejeitarClienteComponent } from './components/rejeitar-cliente/rejeitar-cliente.component';
-
-
 
 @NgModule({
   declarations: [
     HomeGerenteComponent,
-    RejeitarClienteComponent
   ],
   imports: [
     CommonModule
