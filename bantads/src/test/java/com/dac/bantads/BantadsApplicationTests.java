dd1e000a21769b5b1472b6badb8cf4f42075ddcb
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-03-31 23:27:36 -0300

    criacao de projeto springboot

---
+++ bantads/src/test/java/com/dac/bantads/BantadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dac.bantads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
